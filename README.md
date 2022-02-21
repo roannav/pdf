39877d2495263015f7a8bd1ae56d41385fb1099b
Roanna Victorio <roannav@users.noreply.github.com>
2022-02-20 22:55:52 -0800

    cleanup comments

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# pdf
+# get_text_from_PDF.py
+
+Prints the text from a pdf file.  Optionally specify which pages.
+
+Usage:
+py get_text_from_pdf filename.pdf [start_page] [end_page]
+
+start_page and end_page are indexed with the first page being page 1.
+start_page (optional): This is the first page to scan.
+end_page (optional): This is the last page to scan.
+If no end_page argument is given, then start_page is the only page to scan.
+
+Example:  get_text_from_pdf newsletter.pdf 3
+   ==> returns the text from the 3rd page only.
--- get_text_from_PDF.py
+++ get_text_from_PDF.py
@@ -1,15 +1,15 @@
-# print the text from a pdf file.  optionally specify which pages.
+# Prints the text from a pdf file.  Optionally specify which pages.
 #
 # Usage:
 # py get_text_from_pdf filename.pdf [start_page] [end_page]
 #
 # start_page and end_page are indexed with the first page being page 1.
-# end_page (optional): This is the last page to scan
 # start_page (optional): This is the first page to scan.
-#    If no end_page argument is given, then start_page is the only page to scan
+# end_page (optional): This is the last page to scan.
+# If no end_page argument is given, then start_page is the only page to scan.
 #
 # Example:  get_text_from_pdf newsletter.pdf 3
-#    ==> will return the text from the 3rd page only.
+#    ==> returns the text from the 3rd page only.
 
 import PyPDF2
 import sys
